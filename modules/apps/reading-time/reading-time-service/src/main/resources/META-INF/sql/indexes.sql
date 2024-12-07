8d9367985b12e0f3e525b4ecfe03bdab704c898a
amosfong <amos.fong@liferay.com>
2024-12-06 18:19:07 -0800

    Remove more modules

--- modules/apps/reading-time/reading-time-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,2 +0,0 @@
-create unique index IX_12901B5E on ReadingTimeEntry (groupId, ctCollectionId, classNameId, classPK);
-create unique index IX_4FDED5F3 on ReadingTimeEntry (uuid_[$COLUMN_LENGTH:75$], groupId, ctCollectionId);
